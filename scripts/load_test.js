77e4cec7fd8095a5d1c51df912e6903105b2b639
douglas <douglasmartinssantos7@gmail.com>
2024-11-06 12:20:32 -0300

    coments in load test

--- scripts/load_test.js
+++ scripts/load_test.js
@@ -1,4 +1,7 @@
-// O Load Test verifica o comportamento do sistema sob uma carga previsível de usuários.
+/* Um load test (ou teste de carga) é um tipo de teste de performance que mede como uma aplicação ou sistema responde sob uma 
+   quantidade específica de carga ou número de usuários simultâneos. Esse teste simula situações reais para verificar como o sistema
+   lida com o uso normal e com picos de acesso, ajudando a identificar possíveis gargalos e o ponto em que a aplicação começa a
+   desacelerar ou a falhar. */
 
 import http from "k6/http"
 import { sleep, check } from "k6"
@@ -8,11 +11,11 @@ import { BASE_URL } from '../env/base_url.js'
 
 export let options = {
   stages: [
-    { duration: "2m", target: 500 }, // Aumenta gradualmente para 500 usuários em 2 minutos
+    { duration: "2m", target: 500 },  // Aumenta gradualmente para 500 usuários em 2 minutos
     { duration: "3m", target: 1000 }, // Aumenta gradualmente para 1000 usuários
     { duration: "5m", target: 2000 }, // Mantém 2000 usuários por 5 minutos
     { duration: "2m", target: 2000 }, // Mantém os 2000 usuários para observar estabilidade
-    { duration: "1m", target: 0 }, // Reduz para 0 ao final do teste
+    { duration: "1m", target: 0 },    // Reduz para 0 ao final do teste
   ],
   thresholds: {
     http_req_duration: ["p(95)<500"], // 95% das respostas devem ser mais rápidas que 500ms
